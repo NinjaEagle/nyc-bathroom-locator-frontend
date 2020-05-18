008e54c11e3521aea03bb2db8221d6c764eecae6
Kevin Wang <kcwang06@gmail.com>
2020-05-18 18:16:41 -0400

    restroom favorites refactored

--- src/components/HomeMap.js
+++ src/components/HomeMap.js
@@ -1,83 +1,87 @@
-import React, {  useState } from "react";
-import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
-import mapStyles from "./mapStyles";
+import React, { useState } from 'react'
+import {
+	GoogleMap,
+	withScriptjs,
+	withGoogleMap,
+	Marker,
+	InfoWindow,
+} from 'react-google-maps'
+import mapStyles from './mapStyles'
 
-  function Map(props) {
-    const [selectedSpot,setSelectedSpot] = useState(null);
-
-    console.log(selectedSpot)
-
-    return (
-      <GoogleMap
-        defaultZoom={13.5}
-        defaultCenter={{ lat: props.coordinates.lat, lng: props.coordinates.lng }}
-        defaultOptions={{ styles: mapStyles}}
-      >
-        {props.restrooms.map(restroom => (
-          <Marker
-            key={restroom.id}
-            position={{
-              lat: restroom.latitude,
-              lng: restroom.longitude
-            }}
-            onClick={() => {
-              setSelectedSpot(restroom);
-            }}
-            icon={{
-              url: "/unisex.svg",
-              scaledSize: new window.google.maps.Size(25, 25)
-            }}
-            animation={window.google.maps.Animation.DROP}
-          />
-        ))}
-
-        {selectedSpot && (
-          <InfoWindow
-            visible={true}
-            position={{
-              lat: parseFloat(selectedSpot.latitude),
-              lng: parseFloat(selectedSpot.longitude)
-            }}
-            onCloseClick={() => {
-              setSelectedSpot(null);
-            }}
-          >
-            <div>
-              <h2>{selectedSpot.name}</h2>
-              <p>{selectedSpot.address}</p>
-              <p>Type: {selectedSpot.restroom_type}</p>
-              <p>Wheelchair Accessible: {selectedSpot.wheelchair_accessible}</p>
-              <p>Hours: {selectedSpot.start_time}-{selectedSpot.end_time}</p>
-              <p>Save it to your selected list? <button onClick={() => {props.addFave(selectedSpot)}}>Yes!</button></p>
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-  );
-  }
-
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-export default function HomeMap(props){
-  return (
-    <div className="map">
-      <WrappedMap
-        googleMapURL={
-          `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
-        }
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        restrooms={props.allRestrooms}
-        selectedMarker={props.selectedMarker}
-        addFave={props.addFave}
-        hovered={props.hovered}
-        coordinates={props.coordinates}
-      />
-    </div>
-  );
+function Map(props) {
+	const [selectedSpot, setSelectedSpot] = useState(null)
+	return (
+		<GoogleMap
+			defaultZoom={13.5}
+			defaultCenter={{ lat: props.coordinates.lat, lng: props.coordinates.lng }}
+			defaultOptions={{ styles: mapStyles }}>
+			{' '}
+			{props.restrooms.map((restroom) => (
+				<Marker
+					key={restroom.id}
+					position={{
+						lat: restroom.latitude,
+						lng: restroom.longitude,
+					}}
+					onClick={() => {
+						setSelectedSpot(restroom)
+					}}
+					icon={{
+						url: '/unisex.svg',
+						scaledSize: new window.google.maps.Size(25, 25),
+					}}
+					animation={window.google.maps.Animation.DROP}
+				/>
+			))}
+			{selectedSpot && (
+				<InfoWindow
+					visible={true}
+					position={{
+						lat: parseFloat(selectedSpot.latitude),
+						lng: parseFloat(selectedSpot.longitude),
+					}}
+					onCloseClick={() => {
+						setSelectedSpot(null)
+					}}>
+					<div>
+						<h2> {selectedSpot.name} </h2> <p> {selectedSpot.address} </p>{' '}
+						<p> Type: {selectedSpot.restroom_type} </p>{' '}
+						<p> Wheelchair Accessible: {selectedSpot.wheelchair_accessible} </p>{' '}
+						<p>
+							Hours: {selectedSpot.start_time} - {selectedSpot.end_time}{' '}
+						</p>{' '}
+						<p>
+							Save it to your selected list ?{' '}
+							<button
+								onClick={() => {
+									props.addFave(selectedSpot)
+								}}>
+								Yes!
+							</button>{' '}
+						</p>{' '}
+					</div>{' '}
+				</InfoWindow>
+			)}{' '}
+		</GoogleMap>
+	)
 }
 
+const WrappedMap = withScriptjs(withGoogleMap(Map))
 
-
+export default function HomeMap(props) {
+	return (
+		<div className='map'>
+			<WrappedMap
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
+				loadingElement={<div style={{ height: `100%` }} />}
+				containerElement={<div style={{ height: `100%` }} />}
+				mapElement={<div style={{ height: `100%` }} />}
+				restrooms={props.allRestrooms}
+				selectedMarker={props.selectedMarker}
+				addFave={props.addFave}
+				hovered={props.hovered}
+				coordinates={props.coordinates}
+			/>{' '}
+		</div>
+	)
+}
--- src/components/RestroomFavorites.js
+++ src/components/RestroomFavorites.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React from 'react'
 
 class RestroomFavorites extends React.Component {
+	showFavorites = () => {
+		return this.props.faveSpots.map((restroom) => {
+			const {
+				name,
+				restroom_type,
+				wheelchair_accessible,
+				address,
+				start_time,
+				end_time,
+			} = restroom.restroom
+			return (
+				<div key={restroom.id} className='favorite-item'>
+					<h3 className='favoritetitle'>{name}</h3>
+					<p>{address}</p>
+					<p>Restroom Type: {restroom_type}</p>
+					<p>Wheelchair Accesible? {wheelchair_accessible}</p>
+					<p>
+						Hours: {start_time}-{end_time}
+					</p>
+					<button
+						onClick={(event) => {
+							this.props.deleteFave(restroom)
+						}}>
+						Remove
+					</button>
+					<br></br>
+				</div>
+			)
+		})
+	}
 
-  showFavorites = () => {
-    return this.props.faveSpots.map(restroom => {
-      return (
-        <div class="favorite-item">
-            <h3 className="favoritetitle">{restroom.restroom.name}</h3>
-            <p>{restroom.restroom.address}</p>
-            <p>Restroom Type: {restroom.restroom.restroom_type}</p>
-            <p>
-              Wheelchair Accesible? {restroom.restroom.wheelchair_accessible}
-            </p>
-            <p>
-              Hours: {restroom.restroom.start_time}-{restroom.restroom.end_time}
-            </p>
-            <button
-              onClick={event => {
-                this.props.deleteFave(restroom);
-              }}
-            >
-              Remove
-            </button>
-          <br></br>
-        </div>
-      );
-    });
-  };
-
-  render() {
-
-    return (
-      <div className="favorites">
-        
-        <div className="favorites-list">
-          {this.showFavorites()}
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='favorites'>
+				<div className='favorites-list'>{this.showFavorites()}</div>
+			</div>
+		)
+	}
 }
 
-export default RestroomFavorites;
+export default RestroomFavorites
